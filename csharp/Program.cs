119b22dfefd485930f88c5d5bb4414a3047e1392
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-02-25 06:35:53 -0300

    Exercicios com os alunos do desafio 21 dias de ruby

---
+++ csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string a = "";
+           bool b = true;
+           double c = 1.5;
+           int d = 1;
+        }
+    }
+}
---
+++ ruby/exec.rb
@@ -0,0 +1,11 @@
+# Dado que eu tenha uma variável com o numero 360 
+# qual seria o resultado da soma da variável b que tem armazendo 44 somado 
+# com a variável anterior
+
+puts "Digite um numero para a operação matemática de soma com o número 44"
+a = gets.to_i
+b = 44
+c = a + b
+
+puts "O resultado da operação é: #{c}"
+
